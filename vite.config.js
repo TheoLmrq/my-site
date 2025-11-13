24e61c2fa5a633aec8ac2913da8b79dc9af0da1d
Theo <b00832023@essec.edu>
2025-11-13 14:27:38 +0100

    Fix vite.config.js base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // Configuration pour GitHub Pages
 export default defineConfig({
   plugins: [react()],
-  base: '/my-site/', //my-site 
+  base: '/my-site/', // my-site
 })
-
