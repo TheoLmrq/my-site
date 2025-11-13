8bf053d6c3ee9828eaa0a7e1957d33570245e48b
Theo <b00832023@essec.edu>
2025-11-13 14:25:03 +0100

    Set Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Configuration pour GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  base: '/my-site/', //my-site 
 })
+
